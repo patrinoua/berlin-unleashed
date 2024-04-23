fb207b8f72a2b87c0c3d2f210e6ad94784eb45db
Angela Patrinou <patrinoua@gmail.com>
2024-04-24 00:07:45 +0200

    add all the content !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,98 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          exw arxisei kai tsatizomai Get started by editinggggg&nbsp;
-          <code className='font-mono font-bold'>src/app/page.tsx</code>
-        </p>
-        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
-          <a
-            className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Byyyyy{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className='dark:invert'
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className='flex min-h-screen flex-col items-center justify-between bg-black p-4 text-white font-sans tracking-wide'>
+      {/* <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black  lg:static lg:size-auto lg:bg-none'></div>
+      </div> */}
+      <p className='text-4xl m-7'>BERLIN UNLEASHED.</p>
+      <div
+        className='my-30 h-1 w-80p border-t border-gray-300 dark:border-gray-100'
+        style={{
+          margin: 10,
+          height: 1,
+          width: '80%',
+          backgroundColor: 'white',
+          opacity: 0.1,
+        }}
+      ></div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
+      <div
+        // className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left '
+        className='mb-32 grid text-center grid lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2'
+      >
+        <Option
+          text='Discover local events'
+          imageURL='/images/vektor.png'
+          subtext={`Personalized suggestions according to your interests on the days you are visiting`}
         />
-      </div>
 
-      <div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <Option
+          text='ART exhibitions'
+          imageURL='/images/art.png'
+          subtext={`Find out what's going on in your city`}
+        />
 
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className='mb-3 text-2xl font-semibold'>
-            Deploy{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className='m-0 max-w-[30ch] text-balance text-sm opacity-50'>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Option
+          text='Restaurants, bars'
+          imageURL='/images/map.png'
+          subtext={`Get a list of all our restaurant and bar recommendations in berlin along
+        with our favorite highlights`}
+        />
+        <Option
+          text='Find underground events'
+          imageURL='/images/kraftwerk.png'
+          subtext={`Find out what's going on in the city`}
+        />
+        <Option
+          text='Book a tour'
+          imageURL='/images/market.png'
+          subtext={`Get personalised tips or book a tour to see Berlin with your own eyes. We'll talk about your interests and create the perfect tour for you`}
+        />
+        <Option
+          text='Join the newsletter'
+          imageURL='/images/newsletter.png'
+          subtext={`join the newsletter and discover weekly events in the city`}
+        />
+        <Option
+          text='Get the list of resources'
+          imageURL='/images/resources.png'
+          subtext={`Get a list of resources and discover for your self`}
+        />
+        <Option
+          text='Cinema recommendations'
+          imageURL='/images/kino.png'
+          subtext={`Get a list of the best cinemas in Berlin and which movies they are playing`}
+        />
+        <Option
+          text='Buy local art'
+          imageURL='/images/local_art.png'
+          subtext={`Get a list of the best cinemas in Berlin and which movies they are playing`}
+        />
+        <p>Impressium</p>
+        <p>Contact</p>
       </div>
     </main>
+  )
+}
+type OptionProps = {
+  text?: string
+  imageURL?: any //fix this
+  subtext?: string
+}
+const Option = ({ text = 'text', imageURL, subtext }: OptionProps) => {
+  return (
+    <div className='m-5 align-center '>
+      <span className='text-2xl m-4'>{text}</span>
+      <div className='m-5 w-full h-[500px] mx-auto overflow-hidden flex items-center justify-center'>
+        <img
+          src={imageURL}
+          className='h-auto w-full '
+          alt={text.replace(' ', '_')}
+          style={{ objectPosition: ' 50% 50%' }}
+        />
+      </div>
+      <p className='m-6 text-lg'>{subtext}</p>
+    </div>
   )
 }
